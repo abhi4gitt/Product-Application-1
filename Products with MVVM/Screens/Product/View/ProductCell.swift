1fedee44c700ee6fc2eeaf440a7413afc28fe3f3
abhi4gitt <raiabhishek631@gmail.com>
2025-08-03 22:03:08 +0530

    Add ProductCell XIB and connect UI components via IBOutlet

---
+++ Products with MVVM/Screens/Product/View/ProductCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProductCell.swift
+//  Products with MVVM
+//
+//  Created by Abhishek on 03/08/25.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    @IBOutlet weak var productBackgroundView: UIView!
+    @IBOutlet weak var productTitleLabel: UILabel!
+    @IBOutlet weak var productCategoryLabel: UILabel!
+    @IBOutlet weak var rateButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
